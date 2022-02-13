315a9877dccfe191370be30daf0a95fab43d4218
NTHUB2 <98631804+NTHUB2@users.noreply.github.com>
2022-02-13 12:10:14 +0700

    Update lodsting.lua

--- lodsting.lua
+++ lodsting.lua
@@ -3,7 +3,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/NTHUB2/Testing/main/g
 -----------------------------------------------------------------------------------------------------------------------------------
 
 --Jailbreak
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NTHUB2/Testing/main/Jailbreak.t.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NTHUB2/Testing/main/Jailbreak.lua"))();
 -----------------------------------------------------------------------------------------------------------------------------------
 
 
